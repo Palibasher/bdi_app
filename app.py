ea9edff84440954cbcbc01b7913741adbd87f6fe
Palibasher <107269682+Palibasher@users.noreply.github.com>
2025-04-09 12:28:27 +0300

    Update app.py
    mean, new structure

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import timedelta
-from bdi_plot_maker import plot_ffa_forecast
+from bdi_plot_maker import FFAForecastPlotter
 
 
 @st.cache_data
@@ -11,6 +11,7 @@ def load_and_process_data(uploaded_file):
     df = pd.read_excel(uploaded_file, parse_dates=['ArchiveDate', 'StartDate'])
     df['ArchiveDate'] = pd.to_datetime(df['ArchiveDate'], format='%Y-%m-%d', errors='coerce')  # Приводим к единому типу
     return df
+
 
 st.markdown("""
     <style>
@@ -27,15 +28,15 @@ st.markdown("""
 def empty_date_checker(df, selected_date):
     """Проверяет, есть ли данные для выбранной даты."""
     selected_date = pd.to_datetime(selected_date)  # Приводим к единому типу
-    return not df[(df['GroupDesc'] == 'C5TC FACT') & (df['ArchiveDate'] == selected_date)].empty
+    return not df[(df['GroupDesc'] == 'BFA Cape') & (df['ArchiveDate'] == selected_date)].empty
 
 
 # Загрузка файла
 uploaded_file = st.file_uploader("Загрузите Excel-файл", type=["xlsx"])
 
-col1, col2 = st.columns([1.7, 1])
-with col1:
-    if uploaded_file:
+col1, col2, col3 = st.columns([1.7, 1, 0.5])
+if uploaded_file:
+    with col1:
         df = load_and_process_data(uploaded_file)
         df['ArchiveDate'] = pd.to_datetime(df['ArchiveDate'], format='%Y-%m-%d',
                                            errors='coerce')  # Убедимся, что тип datetime
@@ -43,7 +44,7 @@ with col1:
         min_date = df.loc[df['GroupDesc'] == 'BFA Cape', 'ArchiveDate'].min()
         max_date = df.loc[df['GroupDesc'] == 'BFA Cape', 'ArchiveDate'].max()
         # Выбор режима
-        mode = st.radio("**Выберите режим**", ["Одна дата", "Несколько дат"])
+        mode = st.radio("**Выберите режим**", ["Одна дата", "Несколько дат", "Месяц целиком"])
 
         if mode == "Одна дата":
             selected_date = st.date_input("**Выберите дату**", min_value=min_date, max_value=max_date)
@@ -70,23 +71,43 @@ with col1:
             available_months = sorted(df['ArchiveDate'].dt.strftime('%Y-%m').unique(), reverse=True)
             selected_months = st.multiselect("Выберите месяц", available_months)
 
-            available_dates = df[df['ArchiveDate'].dt.strftime('%Y-%m').isin(selected_months)]['ArchiveDate'].unique()
+            available_dates = df[
+                (df['GroupDesc'] == 'BFA Cape') &
+                (df['ArchiveDate'].dt.strftime('%Y-%m').isin(selected_months))
+                ]['ArchiveDate'].unique()
             selected_dates = st.multiselect("Выберите даты", sorted(available_dates),
                                             format_func=lambda x: x.strftime('%Y-%m-%d'))
             if not selected_dates:
                 st.write("⚠️ Выберите хотя бы одну дату.")
                 st.stop()
 
+
+        elif mode == "Месяц целиком":
+            # Выбор месяца
+            available_months = sorted(df['ArchiveDate'].dt.strftime('%Y-%m').unique(), reverse=True)
+            selected_months = st.multiselect("Выберите месяц", available_months)
+
+            if selected_months:
+                # Для выбранного месяца находим все даты
+                selected_month = selected_months[0]  # Берем первый выбранный месяц
+                available_dates = df[df['ArchiveDate'].dt.strftime('%Y-%m') == selected_month]['ArchiveDate'].unique()
+
+                # Отображаем все даты для выбранного месяца
+                selected_dates = sorted(available_dates)
+
+
+            else:
+                selected_dates = []
         forecast_types = st.multiselect("**Выберите тип прогноза**",
-                                            ['Monthly Contract (MON)', 'Quarterly Contract (Q)',
-                                             'Calendar Year Contract (CAL)'],
-                                            default=['Monthly Contract (MON)'])
+                                        ['Monthly Contract (MON)', 'Quarterly Contract (Q)',
+                                         'Calendar Year Contract (CAL)'],
+                                        default=['Monthly Contract (MON)'])
         if not forecast_types:
             st.warning("⚠️ Пожалуйста, выберите хотя бы один тип прогноза.")
             st.stop()  # Останавливает дальнейшее выполнение кода, если нет выбора
 
-with col2:
-    if uploaded_file:
+
+    with col2:
         # Заголовок
         st.markdown("**Настройки скользящих средних**")
 
@@ -116,12 +137,16 @@ with col2:
                 "Среднеквадратическое отклонение (окно 200 дней)",
                 value=False,  # Если sma_90 не выбрана, то сбрасываем галочку
                 disabled=True  # И делаем недоступным
-            )        # Чекбоксы для экспоненциальных скользящих средних
+            )  # Чекбоксы для экспоненциальных скользящих средних
         ewma_90 = st.checkbox("90-дневная экспоненциальная скользящая средняя", value=False)
         ewma_30 = st.checkbox("30-дневная экспоненциальная скользящая средняя", value=False)
 
+    with col3:
+        st.markdown("Доп.настройки")
 
-if uploaded_file:
+        average_forcast_mode = st.checkbox("Усреднить прогнозы", value=True if mode == "Месяц целиком" else False)
+
+
     start_date, end_date = st.slider(
         "**Выберите диапазон дат**",
         min_value=min_date.date(),
@@ -129,19 +154,27 @@ if uploaded_file:
         value=(min_date.date(), max_date.date(),
                ))
     historical_data_types = st.multiselect("**Исторические данные**",
-                                    ['C5TC FACT', 'P5TC FACT',
-                                     'Brent Oil','C5TC / P5TC'],
-                                    default=['C5TC FACT'])
+                                           ['C5TC FACT', 'P5TC FACT',
+                                            'Brent Oil', 'C5TC / P5TC'],
+                                           default=['C5TC FACT'])
     if "Brent Oil" in historical_data_types and "C5TC / P5TC" in historical_data_types:
         st.warning(
             "Нельзя выбрать одновременно 'Brent Oil' и 'C5TC / P5TC'. Оставлена только первая выбранная метрика.")
         historical_data_types.remove("C5TC / P5TC")  # Убираем C5TC / P5TC
-    if ("C5TC / P5TC" in historical_data_types) and ("C5TC FACT" not in historical_data_types or 'P5TC FACT' not in historical_data_types):
+    if ("C5TC / P5TC" in historical_data_types) and (
+            "C5TC FACT" not in historical_data_types or 'P5TC FACT' not in historical_data_types):
         st.warning("Для расчета C5TC / P5TC необходимо выбрать оба показателя: C5TC FACT и P5TC FACT.")
         st.stop()
 
     # Построение графика после выбора параметров
 if uploaded_file:
-    if st.button("Построить график") and selected_dates:
-        plot_ffa_forecast(df,historical_data_types, forecast_types, selected_dates, start_date, end_date, sma_90, sma_200, ewma_30, ewma_90,
-                          rolling_std_90, rolling_std_200)
+    # if st.button("Построить график") and selected_dates:
+    #     plot_ffa_forecast(df,historical_data_types, forecast_types, selected_dates, start_date, end_date, sma_90, sma_200, ewma_30, ewma_90,
+    #                       rolling_std_90, rolling_std_200)
+    plotter = FFAForecastPlotter(df, sma_90=sma_90,
+                                 sma_200=sma_200,
+                                 ewma_30=ewma_30,
+                                 ewma_90=ewma_90,
+                                 rolling_std_90=rolling_std_90,
+                                 rolling_std_200=rolling_std_200)
+    plotter.plot_forecast(historical_data_types, forecast_types, selected_dates, start_date, end_date, average_forcast_mode)
